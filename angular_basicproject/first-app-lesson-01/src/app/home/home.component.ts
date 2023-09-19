a03709cd53e83bc6151fc7c6a66d5b21fe073710
mmmjunjoy <sjbssk0809@gmail.com>
2023-09-19 11:48:24 +0900

    feat:create interface

--- angular_basicproject/first-app-lesson-01/src/app/home/home.component.ts
+++ angular_basicproject/first-app-lesson-01/src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
-
+import { Housinglocation } from '../housinglocation';
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -17,4 +17,15 @@ import { HousingLocationComponent } from '../housing-location/housing-location.c
     </section> `,
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  housingLocation: Housinglocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: 'assets/example-house.jpg',
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
+}
