1bebe87bd7980b1f915ed138f82086bfe3372420
mmmjunjoy <sjbssk0809@gmail.com>
2023-10-10 16:22:38 +0900

    Building CreateToDo API

---
+++ django_study/views_todo.py
@@ -0,0 +1,95 @@
+from django.shortcuts import render
+from django.http import HttpResponse ,JsonResponse
+from .models import TodoModel
+import urllib.request
+import json
+
+
+
+def index(request):
+    return HttpResponse("Hello, world. ")
+
+
+# 0. todo data front에 보내주기
+
+# todo/sendtodo
+def sendtododb(request):
+
+    if request.method == 'GET':
+
+        print("get_success_sendtododata")
+
+
+# 1. 할일 생성
+
+# todo/create
+
+def todocreate(request):
+
+    if request.method == 'POST':
+
+        print("post_success_create")
+
+        # 프론트에서 todo data 받아서 변수에 저장
+
+        TodoPayload = json.loads(request.body)
+        print("payload" , TodoPayload)
+        tdstatus = TodoPayload["tdstatus"]
+        tdtitle = TodoPayload["tdtitle"]
+        tdcontent = TodoPayload["tdcontent"]
+        tdduedate = TodoPayload["tdduedate"]
+
+
+        print("todo data: " , tdstatus , tdtitle , tdcontent, tdduedate )
+
+
+        # todo data저장
+
+        new_todo = TodoModel.objects.create(status = tdstatus , title =tdtitle , content = tdcontent, due_date=tdduedate )
+
+        print("새로운 todo저장되었습니다.")
+
+        successdata = {
+            'result' : True
+        }
+
+        return JsonResponse(successdata)
+        
+
+
+
+
+
+
+
+# 2. 할일 삭제
+
+# todo/delete
+
+def tododelete(request):
+
+    if request.method == 'POST':
+
+        print("post_success_delete")
+
+
+        
+
+
+
+
+# 3. 할일 수정
+
+# todo/modify
+
+
+def todomodify(request):
+
+    if request.method == 'POST':
+
+        print("post_success_modify")
+
+
+
+
+
