daf91ac6c98f808665d52ad80f5ab27a2f020f95
mmmjunjoy <sjbssk0809@gmail.com>
2023-10-17 15:55:31 +0900

    building session_service [set,get,remove]

--- saladlab_angular/session.service.ts
+++ saladlab_angular/session.service.ts
@@ -6,14 +6,17 @@ import { Injectable } from "@angular/core";
 export class SessionService {
   constructor() {}
 
+  // storage에 사용할 정보 저장
   setInfo(userid: string) {
     localStorage.setItem("USERNAME_ID", userid);
   }
 
+  // storage 정보 이용
   getInfo() {
     return localStorage.getItem("USERNAME_ID");
   }
 
+  // 로그아웃
   logOut() {
     localStorage.removeItem("USERNAME_ID");
   }
