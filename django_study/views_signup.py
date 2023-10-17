1e7a6577e1313a9ae1556cbcebd1f34da628a31e
mmmjunjoy <sjbssk0809@gmail.com>
2023-10-17 14:00:55 +0900

    modify login API (+) post [userid] for session

--- django_study/views_signup.py
+++ django_study/views_signup.py
@@ -17,7 +17,7 @@ def signup(request):
   if request.method == 'POST':
 
     # POST처리 성공여부 확인
-    print("success")
+    print("signup_success")
 
     # try:
  
@@ -27,17 +27,17 @@ def signup(request):
 
 
     #프론트에서 데이터 받아서 저장
-    payload = json.loads(request.body)
-    print("payload" , payload)
-    username = payload["username"]
-    password = payload["password"]
+    SignupPayload = json.loads(request.body)
+    print("payload" , SignupPayload)
+    username = SignupPayload["username"]
+    password = SignupPayload["password"]
 
 
     print("test", username , password)
 
     # ---회원삭제---
 
-    # deletes = UserSignup.objects.filter(id = '4')
+    # deletes = UserSignup.objects.all()
     # deletess = deletes.delete()
    
 
@@ -46,6 +46,20 @@ def signup(request):
     # 1.1 현재 table의 username 모두 가져오기
     usernames = UserSignup.objects.all().values_list('user_name' , flat = True)
     print(usernames)
+
+
+    # ---프론트에게 던져줄 값---
+
+    success = {
+      'result': True
+
+    }
+
+    fail  = {
+      'result' : False
+    }
+
+    print("프론트값전달" , success, fail)
     
     # 1.2 중복체크 통과 -> 저장  , 불통 -> 경고메세지
     for i in usernames:
@@ -53,13 +67,13 @@ def signup(request):
 
         # 불통 -> 경고메세지 실행 및 http 오류 처리
         print("---error: 사용자 정보가 중복으로 존재합니다---")
-        return HttpResponse("NotSupportService", status = 200)
+        return JsonResponse(fail )
 
 
     # 통과 -> 저장 및 확인메세지 출력
     new_user = UserSignup.objects.create(user_name= username , password =password )
     print("---success: 저장이 되었습니다---")
-    return HttpResponse("GOOD SIGNUP" , status = 200)
+    return JsonResponse(success )
 
     
        
@@ -67,23 +81,69 @@ def signup(request):
     # except Exception as e:
     #   print("error", e)
 
-
-        
-
-   
-   
-
       
-    
-      
-    
-    
-    
-    # return redirect('/api/signin')
-  
+   
 
 # 2. 로그인
-#POST. http://{host}/api/user/login & payload: {user_name: "USER_NAME", password: "PASSWORD" }
+# user/login
 
-# def signin(request):
-#   if request.method
+
+def login(request):
+  
+  if request.method == 'POST':
+
+    print("로그인 통신")
+
+    #0. front에서 사용자가 기입한 username과 password값 
+
+    LoginPayload = json.loads(request.body)
+    print("front data: ", LoginPayload)
+    LoginUsername = LoginPayload["loginusernames"]
+    print("사용자입력id : " ,LoginUsername)
+    LoginPassword = LoginPayload["loginpasswords"]
+    print("사용자입력pw : " ,LoginPassword)
+
+    #1.DB username 확인 
+
+    UsernameSet = UserSignup.objects.all().values_list("user_name" , flat = True)
+    print(UsernameSet)
+
+    #2.DB password 확인
+
+    PasswordSet = UserSignup.objects.all().values_list("password", flat= True)
+    print(PasswordSet)
+
+    #2-2 -> session이용하기 위한 해당 username에 대한 userid값 넘겨주기
+    Userids = UserSignup.objects.filter(user_name = LoginUsername).values("id")
+
+    print("USERID:" , Userids)
+
+    #3.로그인 본 동작
+
+    loginsuccess = {
+      "result" : True,
+      "resUserid" : Userids[0]["id"]
+    }
+
+    loginpasswordfail = {
+      "result" : False
+    }
+
+    loginusernamefail = {
+      "result" : False
+    }
+
+
+    for i in UsernameSet:
+      if i == LoginUsername:
+        for j in PasswordSet:
+          if j == LoginPassword:
+            return JsonResponse(loginsuccess )
+        return JsonResponse(loginpasswordfail)
+      
+    
+    return JsonResponse(loginusernamefail)
+  
+
+          
+    
