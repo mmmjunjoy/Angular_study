5f0d1de3a5981bbb3250ad80030b953b390c934d
mmmjunjoy <sjbssk0809@gmail.com>
2024-02-22 15:09:37 +0900

    findindex  & push

---
+++ Angular_study/let.ts
@@ -0,0 +1,33 @@
+
+
+
+// findindex ?
+
+// 이것은, 배열에서 해당값의 인덱스를 찾는것이다.
+// 이또한 없으면, -1 이 반환된다.
+
+
+
+// push?
+
+// 이것은 배열에 값을 추가하는 것이다.
+
+
+
+
+letfunctions(){
+        for(let i = 0 ; i < this.one.selected.length; i++){
+            let item = this.one.selected[i]
+            if(this.search_type == 'product'){
+                let idx = this.one_product[this.search_type].findIndex((product)=>{
+                    return product.id == item.id
+                })
+                if(idx == -1){
+                    this.one_product[this.search_type].push(item)
+                    
+                }
+            }
+
+        }
+
+    }
